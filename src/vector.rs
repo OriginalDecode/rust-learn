9c73996256b39b07780bd71bcf28349db63e5fbe
OriginalDecode <originaldecode@outlook.com>
2018-11-13 13:36:55 +0100

    functional vector

--- src/vector.rs
+++ src/vector.rs
@@ -1,24 +1,19 @@
-
-// use std::ops::{ Add, Mul, Neg, Sub, Div };
-pub type Vector2<T> =[T;2];
-
-impl<T> Add for Vector2<T> {
-    type Output = Vector2<T>;
-
-    fn add(self, _rhs: Vector2<T> ) -> Vector2<T> {
-        Vector2([self[0] + _rhs[0], self[1] + _rhs[1]])
-    }
+use std::ops;
+pub struct Vector2<T>
+{
+    x: T,
+    y: T,
 }
 
-// impl<T> Vector2<T> {
-//     fn new( _x : T, _y : T) -> Vector2<T> {
-//         Vector2<T> { x : _x, y : _y }
-//     }
-// }
+impl<T> ops::Add<Vector2<T>> for Vector2<T> 
+where T: ops::Add<T, Output=T>
+{
+    type Output = Vector2<T>;
 
-// impl<T> std::ops::Add<Vector2<T>> for Vector2<T> {
-//     type Output = Vector2<T>;
-//     fn add(self, _rhs: Vector2<T>) -> Vector2<T> {
-//         return Vector2 { x : self.x + _rhs.x, y : self.y + _rhs.y };
-//     }
-// }
+    fn add(self, rhs: Vector2<T> ) -> Vector2<T> {
+        Vector2::<T> {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
