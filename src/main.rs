c75b0e9fd856acc6959a57cff8506bd6fd7db569
OriginalDecode <originaldecode@outlook.com>
2018-11-13 15:40:55 +0100

    added other operators and derived copy & clone on vector

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,12 @@ fn main() {
     let _vec20 = Vector2{x:31.0, y:13.0};
     let _vec21 = Vector2{x:42.0, y:13.0};
 
-    let _vec22 = _vec20 + _vec21;
+    let add_vec22 = _vec20 + _vec21;
+    let sub_vec22 = _vec20 - _vec21;
+    let mul_vec22 = _vec20 * _vec21;
 
-    println!("{}, {}", _vec22.x, _vec22.y);
+    println!("{}, {}", add_vec22.x, add_vec22.y);
+    println!("{}, {}", sub_vec22.x, sub_vec22.y);
+    println!("{}, {}", mul_vec22.x, mul_vec22.y);
 
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,10 +1,11 @@
-use std::ops;
+#[derive(Copy, Clone)]
 pub struct Vector2<T>
 {
     x: T,
     y: T,
 }
 
+use std::ops;
 impl<T> ops::Add<Vector2<T>> for Vector2<T> 
 where T: ops::Add<T, Output=T>
 {
@@ -17,3 +18,30 @@ where T: ops::Add<T, Output=T>
         }
     }
 }
+
+impl<T> ops::Sub<Vector2<T>> for Vector2<T> 
+where T: ops::Sub<T, Output=T>
+{
+    type Output = Vector2<T>;
+
+    fn sub(self, rhs: Vector2<T> ) -> Vector2<T> {
+        Vector2::<T> {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl<T> ops::Mul<Vector2<T>> for Vector2<T> 
+where T: ops::Mul<T, Output=T>
+{
+    type Output = Vector2<T>;
+
+    fn mul(self, rhs: Vector2<T> ) -> Vector2<T> {
+        Vector2::<T> {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+        }
+    }
+}
+
