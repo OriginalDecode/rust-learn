3fe2492d23ad9002e7a6adae2bee270312eeb257
OriginalDecode <originaldecode@outlook.com>
2019-01-23 12:30:54 +0100

    Added a separate file for macros and removed the .txt file in src

---
+++ src/macros.rs
@@ -0,0 +1,36 @@
+macro_rules! impl_op {
+	($this:ident, $rhs:ident, $vector:ty, $body:block, $fname:ident, $trait:ident) => {
+		impl<T> $trait<$vector> for $vector 
+			where T: $trait<T, Output=T>
+		{
+			type Output = $vector;
+			fn $fname($this, $rhs : $vector ) -> $vector {
+				$body
+			}
+		}
+	};
+}
+
+macro_rules! impl_add {
+	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
+		impl_op!($this, $rhs, $vector, $body, add, Add);
+	};
+}
+
+macro_rules! impl_sub {
+	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
+		impl_op!($this, $rhs, $vector, $body, sub, Sub);
+	};
+}
+
+macro_rules! impl_mul {
+	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
+		impl_op!($this, $rhs, $vector, $body, mul, Mul);
+	};
+}
+
+macro_rules! impl_div {
+	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
+		impl_op!($this, $rhs, $vector, $body, div, Div);
+	};
+}
--- src/vector.rs
+++ src/vector.rs
@@ -2,6 +2,9 @@
 /*
 	Copy and Clone are traits
 */
+
+include!("macros.rs");
+
 #[derive(Copy, Clone)]
 pub struct Vector2<T>
 {
@@ -25,44 +28,6 @@ pub struct Vector4<T>
 	z: T,
 	w: T,
 }
-
-macro_rules! impl_op {
-	($this:ident, $rhs:ident, $vector:ty, $body:block, $fname:ident, $trait:ident) => {
-		impl<T> $trait<$vector> for $vector 
-			where T: $trait<T, Output=T>
-		{
-			type Output = $vector;
-			fn $fname($this, $rhs : $vector ) -> $vector {
-				$body
-			}
-		}
-	};
-}
-
-macro_rules! impl_add {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, add, Add);
-	};
-}
-
-macro_rules! impl_sub {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, sub, Sub);
-	};
-}
-
-macro_rules! impl_mul {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, mul, Mul);
-	};
-}
-
-macro_rules! impl_div {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, div, Div);
-	};
-}
-
 
 use std::ops:: { Add, Mul,  Sub, Div };
 
