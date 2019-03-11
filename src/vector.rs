d7defc00590c714509148b8a971a172e07b486c8
OriginalDecode <originaldecode@outlook.com>
2019-03-11 23:08:28 +0100

    Implemented remaining funcs in vector

--- src/vector.rs
+++ src/vector.rs
@@ -63,6 +63,8 @@ impl_div!(self, rhs, Vector2<T>, {
 	}
 });
 
+
+
 #[inline(always)]
 pub fn vec2_dot<T>( a : Vector2<T>, b : Vector2<T>) -> T 
 	where T: Copy + Add<T, Output=T> + Mul<T, Output=T>
@@ -70,12 +72,90 @@ pub fn vec2_dot<T>( a : Vector2<T>, b : Vector2<T>) -> T
 	a.x * b.x + a.y * b.y
 }
 
+impl_add!(self, rhs, Vector3<T>, {
+	Vector3::<T> 
+	{
+		x : self.x + rhs.x,
+		y : self.y + rhs.y,
+		z : self.z + rhs.z,
+	}
+});
+
+impl_sub!(self, rhs, Vector3<T>, {
+	Vector3::<T> 
+	{
+		x : self.x - rhs.x,
+		y : self.y - rhs.y,
+		z : self.z - rhs.z,
+	}
+});
+
+impl_mul!(self, rhs, Vector3<T>, {
+	Vector3::<T> 
+	{
+		x : self.x * rhs.x,
+		y : self.y * rhs.y,
+		z : self.z * rhs.z,
+	}
+});
+
+impl_div!(self, rhs, Vector3<T>, {
+	Vector3::<T> 
+	{
+		x : self.x / rhs.x,
+		y : self.y / rhs.y,
+		z : self.z / rhs.z,
+	}
+});
+
 #[inline(always)]
 pub fn vec3_dot<T>(a: Vector3<T>, b: Vector3<T>) -> T
 	where T: Copy + Add<T, Output=T> + Mul<T, Output=T>
 {
 	a.x * b.x + a.y * b.y + a.z * b.z
 }
+
+
+impl_add!(self, rhs, Vector4<T>, {
+	Vector4::<T> 
+	{
+		x : self.x + rhs.x,
+		y : self.y + rhs.y,
+		z : self.z + rhs.z,
+		w : self.w + rhs.w,
+	}
+});
+
+impl_sub!(self, rhs, Vector4<T>, {
+	Vector4::<T> 
+	{
+		x : self.x - rhs.x,
+		y : self.y - rhs.y,
+		z : self.z - rhs.z,
+		w : self.w - rhs.w,
+	}
+});
+
+impl_mul!(self, rhs, Vector4<T>, {
+	Vector4::<T> 
+	{
+		x : self.x * rhs.x,
+		y : self.y * rhs.y,
+		z : self.z * rhs.z,
+		w : self.w * rhs.w,
+	}
+});
+
+impl_div!(self, rhs, Vector4<T>, {
+	Vector4::<T> 
+	{
+		x : self.x / rhs.x,
+		y : self.y / rhs.y,
+		z : self.z / rhs.z,
+		w : self.w / rhs.w,
+	}
+});
+
 
 #[inline(always)]
 pub fn vec4_dot<T>( a: Vector4<T>, b: Vector4<T> ) -> T
