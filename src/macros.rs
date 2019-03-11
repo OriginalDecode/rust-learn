aba7d301d1837811e0e505051629d30733d2558d
OriginalDecode <originaldecode@outlook.com>
2019-03-11 17:08:57 +0100

    Updated macro naming, ( vector -> type ) and implemented other operators in matrix4

--- src/macros.rs
+++ src/macros.rs
@@ -1,11 +1,11 @@
 
 macro_rules! impl_op {
-	($this:ident, $rhs:ident, $vector:ty, $body:block, $fname:ident, $trait:ident) => {
-		impl<T> $trait<$vector> for $vector 
+	($this:ident, $rhs:ident, $type:ty, $body:block, $fname:ident, $trait:ident) => {
+		impl<T> $trait<$type> for $type 
 			where T: Copy + $trait<T, Output=T>
 		{
-			type Output = $vector;
-			fn $fname($this, $rhs : $vector ) -> $vector {
+			type Output = $type;
+			fn $fname($this, $rhs : $type ) -> $type {
 				$body
 			}
 		}
@@ -13,25 +13,25 @@ macro_rules! impl_op {
 }
 
 macro_rules! impl_add {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, add, Add);
+	($this:ident, $rhs:ident, $type:ty, $body:block) => {
+		impl_op!($this, $rhs, $type, $body, add, Add);
 	};
 }
 
 macro_rules! impl_sub {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, sub, Sub);
+	($this:ident, $rhs:ident, $type:ty, $body:block) => {
+		impl_op!($this, $rhs, $type, $body, sub, Sub);
 	};
 }
 
 macro_rules! impl_mul {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, mul, Mul);
+	($this:ident, $rhs:ident, $type:ty, $body:block) => {
+		impl_op!($this, $rhs, $type, $body, mul, Mul);
 	};
 }
 
 macro_rules! impl_div {
-	($this:ident, $rhs:ident, $vector:ty, $body:block) => {
-		impl_op!($this, $rhs, $vector, $body, div, Div);
+	($this:ident, $rhs:ident, $type:ty, $body:block) => {
+		impl_op!($this, $rhs, $type, $body, div, Div);
 	};
 }
